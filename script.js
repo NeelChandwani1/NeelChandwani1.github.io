4cedd4bb70357f73754b318e64b732251ef078d9
NeelChandwani1 <neelchandwani09@gmail.com>
2025-03-10 17:35:18 -0400

    update all

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Initialize Particles.js
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('Particles.js loaded!');
+});
+
 // Smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
